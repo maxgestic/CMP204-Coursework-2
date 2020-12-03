e46859003c3f85cf122f3708350178912c9cd853
Maximilian Ring <maxring.work@gmail.com>
2020-12-03 00:14:46 +0000

    Contact Form Created  - Added content to contact.html  - Created clone of contact.html (success.html) with alert saying that the message has been sent after used submits contact form  - Created PHP script that will send email with the contents of the contact form   - Right now this will send the message back to the user   - This can be changed in the code easily   - From address can also be changed in the headers  - Updated style.css to match contact.html and success.html pages

---
+++ php/contact.php
@@ -0,0 +1,88 @@
+<html lang="en">
+
+    <body>
+
+    <?php
+
+        $name = $email = $tel = $message = "";
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+            $name = input($_POST["name"]);
+            $email = input($_POST["email"]);
+            $tel = input($_POST["tel"]);
+            $message = input($_POST["message"]);
+
+            send_mail($name, $email, $tel, $message);
+
+            header("Location: ../success.html");
+
+        }
+
+        function input($data){
+
+            $data = trim($data);
+
+            $data = stripslashes($data);
+
+            $data= nl2br(htmlspecialchars($data));
+
+            return $data;
+
+        }
+
+        function send_mail($name, $email, $tel, $message){
+
+            $to = $email;
+            $subject = "Contact Form Email sent by $name";
+
+            $message = "
+            <html lang='en'>
+            
+                <head>
+                
+                    <title>HTML email</title>
+                    
+                </head>
+                
+                <body style='background-color: black; color: white; padding: 5px'>
+            
+                    <p>
+                    
+                    This is a message sent by $name<br>
+                    <br>
+                    Their Phone Number is: $tel<br>
+                    <br>
+                    Their E-Mail Address is: $email<br>
+                    <br>
+                    Their Message is: <br>
+                    <div style='border-radius: 10px; color: black; background-color: white; padding: 20px; margin: 5px;'>
+                        $message
+                    </div>
+                    <br>
+                    <br>
+                    <br>
+                    This email was sent by an automated contact forum
+                    
+                    </p>
+           
+                </body>
+            </html>
+            ";
+
+            // Always set content-type when sending HTML email
+            $headers = "MIME-Version: 1.0" . "\r\n";
+            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+            // More headers
+            $headers .= 'From: notreal@avicii.com' . "\r\n";
+
+            mail($to,$subject,$message,$headers);
+
+        }
+
+    ?>
+
+    </body>
+
+</html>
