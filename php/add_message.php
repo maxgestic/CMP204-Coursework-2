805791d8489ddc1e81437886430684149261675d
Maximilian Ring <maxring.work@gmail.com>
2020-12-10 17:02:50 +0000

    Started work on the posting messages feature

---
+++ php/add_message.php
@@ -0,0 +1,54 @@
+<html lang="en">
+
+<body>
+
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    session_start();
+
+    include('db.php');
+
+    $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+    if($link === false){
+        die("ERROR: Could not connect. " . mysqli_connect_error());
+    }
+
+
+    $dbh = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_NAME, DB_USERNAME, DB_PASSWORD);
+
+    $message = $_POST["message"];
+
+    $sql = "INSERT INTO messages (user_id,message) VALUES (?, ?)";
+
+    if($stmt = mysqli_prepare($link, $sql)){
+
+        mysqli_stmt_bind_param($stmt, "is", $parm_id, $parm_message);
+
+        $parm_id = $_SESSION['id'];
+        $parm_message = $message;
+
+        if(mysqli_stmt_execute($stmt)){
+
+            header("location: messages.php");
+
+        }
+        else{
+
+            echo "Something went wrong. 1 Please try again later.";
+
+        }
+
+        mysqli_stmt_close($stmt);
+
+    }
+
+}
+
+?>
+
+</body>
+
+</html>
--- php/add_news.php
+++ php/add_news.php
@@ -11,6 +11,8 @@
         include('db.php');
 
 
+
+
         try {
             $dbh = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_NAME, DB_USERNAME, DB_PASSWORD);
 
